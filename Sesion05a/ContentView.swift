c24535eea93be4f086f90caa9b27c5b690bc56d0
AsumaToki04 <megamanzxu@hotmail.com>
2024-12-10 04:22:51 -0800

    Vista ContentView Modificada 1

--- Sesion05a/ContentView.swift
+++ Sesion05a/ContentView.swift
@@ -14,15 +14,47 @@ struct ContentView: View {
         animation: .default
     )
     private var usuarios: FetchedResults<Usuario>
+    @State private var contador = 1
     
     var body: some View {
         VStack {
+            Button(action: {
+                agregarUsuario()
+            }) {
+                Text("Agregar usuario")
+                    .padding()
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            
             List {
                 ForEach(usuarios) { item in
                     VStack(alignment: .leading) {
                         Text(item.nombre ?? "Sin nombre")
                             .font(.headline)
                     }
+                }
+            }
+        }
+        .onAppear {
+            if usuarios.count > 0 {
+                contador = usuarios.count
+            }
+        }
+    }
+    
+    private func agregarUsuario() {
+        viewContext.perform {
+            withAnimation {
+                let nuevoUsuario = Usuario(context: viewContext)
+                nuevoUsuario.id = UUID()
+                nuevoUsuario.nombre = "Usuario \(String(format: "%02d", contador))"
+                do {
+                    try viewContext.save()
+                    contador += 1
+                } catch {
+                    print("Error al guardar: \(error)")
                 }
             }
         }
